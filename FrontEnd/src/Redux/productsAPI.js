ff9e5eca435cc7c051fa43d908dd6e2890ad563a
hudashoeib <hudashoeib@gmail.com>
2026-07-03 10:46:41 +0300

    front end deploy setup

--- FrontEnd/src/Redux/productsAPI.js
+++ FrontEnd/src/Redux/productsAPI.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://brova.onrender.com" }),
   endpoints: (builder) => ({
     getproductsByName: builder.query({
       query: (name) => `products`,
@@ -13,7 +13,7 @@ export const productsApi = createApi({
 });
 export const oneproductApi = createApi({
   reducerPath: "oneproductApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://brova.onrender.com/" }),
   endpoints: (builder) => ({
     getOneProduct: builder.query({
       query: (name) => `products/${name}`,
